aa04f5246a1f5add1df51aedd85536bff994508e
Morgan Ogola <morganogola@gmail.com>
2023-12-05 14:49:00 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_project/pages/splashscreenview.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      );
+  }
+}
+
