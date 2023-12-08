d49a1cd6cec719bfc232fde01d4b738c28de4f79
Morgan Ogola <morganogola@gmail.com>
2023-12-08 11:08:35 +0300

    created a device scanning page extended to the device scanning controller

---
+++ lib/pages/device_scanning.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_project/controllers/device_scanning_controller.dart';
+import 'package:get/get.dart';
+
+class DeviceScanning extends GetView<DeviceScanningController>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+
+}
