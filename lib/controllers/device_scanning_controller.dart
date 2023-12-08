c5cd3ba60b1a15f08217d07a73388271ca6a601e
Morgan Ogola <morganogola@gmail.com>
2023-12-08 12:00:55 +0300

    Added BluetoothManager bluetoothManager = BluetoothManager.instance;

--- lib/controllers/device_scanning_controller.dart
+++ lib/controllers/device_scanning_controller.dart
@@ -1,4 +1,15 @@
-class DeviceScanningController{
+import 'package:flutter_bluetooth_basic/flutter_bluetooth_basic.dart';
 
+class DeviceScanningController{
+  //Bluetooth Instance
+  BluetoothManager bluetoothManager = BluetoothManager.instance;
+
+    //scanning for devices
+    Future <Stream> scanForDevices()async{
+      bluetoothManager.startScan()
+      .then((value) => null);
+      final result = bluetoothManager.scanResults;
+      return result;
+    }
 
 }
